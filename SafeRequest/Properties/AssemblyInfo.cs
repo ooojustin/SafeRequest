1caa9de1371184970c4323f6bc88c0069e6b0551
Justin <justin@garofolo.net>
2018-11-10 05:22:49 -0500

    updated basic assembly info

--- SafeRequest/Properties/AssemblyInfo.cs
+++ SafeRequest/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SafeRequest .NET")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("SafeRequest")]
+[assembly: AssemblyDescription(".NET Web Request Encryption")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SafeRequest .NET")]
+[assembly: AssemblyProduct("SafeRequest")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.6")]
+[assembly: AssemblyFileVersion("1.0.0.6")]
