1b93c6d64fc142b1e8975e8c61f26df6b20919b2
TrapperHell <trapper-hell@hotmail.com>
2019-01-04 17:31:12 +0100

    - Overall refactor and reorganization of code - Replace SafeRequest Test project with a proper UnitTest project

--- SafeRequest.Tests/SafeRequestTests.cs
+++ SafeRequest.Tests/SafeRequestTests.cs
@@ -1,33 +1,33 @@
-﻿using System;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Collections.Specialized;
 
-namespace SafeRequest.Test {
-
-    class Program {
-
+namespace SafeRequest.Tests
+{
+    [TestClass]
+    public class SafeRequestTests
+    {
         private const string URL = "https://example.com/example.php";
         private const string ENCRYPTION_KEY = "your encryption key here";
 
-        static void Main(string[] args) {
-
-            SafeRequest safeRequest = new SafeRequest(ENCRYPTION_KEY);
+        [TestMethod]
+        public void SafeRequestEncryptDecrypt()
+        {
+            var safeRequest = new SafeRequest(ENCRYPTION_KEY);
             Response response;
 
             // POST example
-            NameValueCollection values = new NameValueCollection();
+            var values = new NameValueCollection();
             values["some_key"] = "some_value";
             response = safeRequest.Request(URL, values);
-            Console.WriteLine(response.message);
-            Console.ReadKey();
+            var originalMessage = response.Message;
 
             // GET example
             // Note: Requests with null values still post an authentication key to verify the validity of the request.
             response = safeRequest.Request(URL);
-            Console.WriteLine(response.message);
-            Console.ReadKey();
+            var newMessage = response.Message;
 
+            Assert.IsNotNull(newMessage);
+            Assert.AreEqual(originalMessage, newMessage);
         }
-
     }
-
 }
--- SafeRequest/SafeRequest.cs
+++ SafeRequest/SafeRequest.cs
@@ -4,73 +4,104 @@ using System.Collections.Generic;
 using System.Collections.Specialized;
 using System.Linq;
 using System.Net;
-using System.Security;
 
-namespace SafeRequest {
+namespace SafeRequest
+{
+    public class SafeRequest
+    {
+        public const string UserAgent = nameof(SafeRequest);
+        private readonly Encryption encryption;
 
-    public class SafeRequest {
-
-        public SafeRequest(string key, byte[] iv = null) {
+        public SafeRequest(string key, byte[] iv = null)
+        {
             encryption = new Encryption(key);
-            SetIV(iv);
-        }
 
-        private void SetIV(byte[] iv) {
             if (iv != null)
-                if (iv.Count() == 16)
-                    encryption.SetIV(iv);
+                SetIV(iv);
         }
 
-        public string UserAgent = "SafeRequest";
-        public bool NullifyProxy = true;
+        public bool NullifyProxy { get; set; } = true;
 
-        private Encryption encryption;
-        public string EncryptString(string plainText) { return encryption.EncryptString(plainText); }
-        public string DecryptString(string cipherText) { return encryption.DecryptString(cipherText); }
+        public string DecryptString(string cipherText)
+        {
+            return encryption.DecryptString(cipherText);
+        }
 
-        public Response Request(string url, NameValueCollection values = null) {
-            Response response = new Response(encryption);
-            try {
-                WebClient web = GetClient();
-                if (values == null)
-                    values = new NameValueCollection();
-                Dictionary<string, object> dictionary = GetDictionary(values);
-                if (dictionary.ContainsKey("authentication_key"))
-                    throw new Exception("Key \"authentication_key\" must not be defined.");
-                dictionary.Add("authentication_key", response.EncryptedAuth());
-                string rawUpload = JsonConvert.SerializeObject(dictionary);
-                string upload = encryption.EncryptString(rawUpload);
-                string rawEncrypted = web.UploadString(url, upload);
-                string raw = encryption.DecryptString(rawEncrypted);
-                response.Initialize(raw);
-            } catch (Exception ex) {
-                response.status = false;
-                response.message = ex.Message;
+        public string EncryptString(string plainText)
+        {
+            return encryption.EncryptString(plainText);
+        }
+
+        public WebClient GetClient()
+        {
+            var webClient = new WebClient();
+
+            if (!string.IsNullOrEmpty(UserAgent))
+                webClient.Headers.Add(HttpRequestHeader.UserAgent, UserAgent);
+
+            if (NullifyProxy)
+                webClient.Proxy = null;
+
+            return webClient;
+        }
+
+        public Response Request(string url, NameValueCollection values = null)
+        {
+            var dataDictionary = GetDictionary(values ?? new NameValueCollection());
+            if (dataDictionary.ContainsKey("authentication_key"))
+                throw new Exception("Key \"authentication_key\" must not be defined.");
+
+            var response = new Response();
+            var authKey = GetRandomAuthKey(5);
+            dataDictionary.Add("authentication_key", encryption.EncryptString(authKey));
+
+            try
+            {
+                var rawRequest = JsonConvert.SerializeObject(dataDictionary);
+                var encryptedRequest = encryption.EncryptString(rawRequest);
+
+                var webClient = GetClient();
+                var encryptedResponse = webClient.UploadString(url, encryptedRequest);
+                var rawResponse = encryption.DecryptString(encryptedResponse);
+
+                response.Initialize(rawResponse, authKey);
             }
+            catch (Exception ex)
+            {
+                response.Status = false;
+                response.Message = ex.Message;
+            }
+
             return response;
         }
 
-        public WebClient GetClient() {
-
-            WebClient web = new WebClient();
-
-            if (!string.IsNullOrEmpty(UserAgent))
-                web.Headers.Add(HttpRequestHeader.UserAgent, UserAgent);
-
-            if (NullifyProxy)
-                web.Proxy = null;
-
-            return web;
-
-        }
-
-        private static Dictionary<string, object> GetDictionary(NameValueCollection values) {
+        private static Dictionary<string, object> GetDictionary(NameValueCollection values)
+        {
             var result = new Dictionary<string, object>();
+
             foreach (string key in values.Keys)
                 result.Add(key, values[key]);
+
             return result;
         }
 
-    }
+        private static string GetRandomAuthKey(int keyLength)
+        {
+            var randomizer = new Random();
 
+            var keyChars = Enumerable.Range(1, keyLength)
+                .Select(x => (char)randomizer.Next(33, 127))
+                .ToArray();
+
+            return new string(keyChars);
+        }
+
+        private void SetIV(byte[] iv)
+        {
+            if (iv != null && iv.Length == 16)
+                encryption.SetIV(iv);
+            else
+                throw new ArgumentException($"{nameof(iv)} is invalid.");
+        }
+    }
 }
